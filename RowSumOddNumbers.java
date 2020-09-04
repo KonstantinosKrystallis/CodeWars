1bd57cbcba9e2c478964c67e9a68b725e0570329
Konstantinos Krystallis <konkry1@hotmail.com>
2020-09-05 01:52:56 +0300

    first commit

---
+++ RowSumOddNumbers.java
@@ -0,0 +1,17 @@
+class RowSumOddNumbers {
+    public static int triangleSumOddNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += rowSumOddNumbers(i);
+        return sum;
+    }
+
+    public static int rowSumOddNumbers(int n) {
+        return n * n * n;
+    }
+}
+
+/*
+ * int min = n * (n - 1) + 1; // calculates the min num of the row int max = n *
+ * (n + 1) - 1; // calculates the max num of the row
+ */
---
+++ Solution.java
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/train/java
+
+public class Solution {
+    public static int zeros(int n) {
+        int zeros = 0;
+        for (int i = 5; n / i >= 1; i *= 5)
+            zeros += n / i;
+        return zeros;
+    }
+}
