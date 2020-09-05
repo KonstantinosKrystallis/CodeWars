973aa18fefef5ef8c6d40d842596c1fe390a9943
Konstantinos Krystallis <konkry1@hotmail.com>
2020-09-05 15:30:43 +0300

    Added C# practice

--- SimplePigLatin.java
+++
@@ -1,8 +0,0 @@
-//https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/java
-public class SimplePigLatin {
-    public static String pigIt(String str) {
-        str = str.replaceAll("[^a-zA-Z ]", "");// Create string with only chars a-z, A-Z and whitespaces
-        System.out.println(str);
-        return str;
-    }
-}
