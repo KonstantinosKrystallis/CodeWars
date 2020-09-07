4854e6452a31d9181fc8eceecf5f4030ddbc2320
Konstantinos Krystallis <konkry1@hotmail.com>
2020-09-07 15:11:20 +0300

    Actually added Parentheses.cs & MovingZerosToTheEnd.cs

---
+++ C#/CsPractice/CsPractice/MovingZerosToTheEnd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPractice
+{
+    class MovingZerosToTheEnd
+    {
+        //https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/csharp
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int countNonZeros = 0;//Here we count all the non zero integers
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] != 0) //If the current element is a non zero shift it to the index of count
+                    arr[countNonZeros++] = arr[i];
+            while (countNonZeros < arr.Length) //Now that we found all the non zeros and we know the where the last one is located
+                arr[countNonZeros++] = 0; //We simply fill the rest of the array with zeros.
+            return arr;
+        }
+
+        public static int[] MoveZeroes2(int[] arr)
+        {
+            return arr.OrderBy(x => x == 0).ToArray();
+        }
+    }
+}
---
+++ C#/CsPractice/CsPractice/Parentheses.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPractice
+{
+    public class Parentheses
+    {
+        //https://www.codewars.com/kata/52774a314c2333f0a7000688/train/csharp
+        public static bool ValidParentheses(string input)
+        {
+            char[] inputString = input.ToCharArray();
+            int countOpenParenthesis = 0;
+            int countClosedParenthesis = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '(')
+                {
+                    countOpenParenthesis++;
+                }
+                else if (inputString[i] == ')')
+                {
+                    countClosedParenthesis++;
+                }
+                if (countClosedParenthesis > countOpenParenthesis)
+                {
+                    return false;
+                }
+            }
+            if (countOpenParenthesis == countClosedParenthesis)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool ValidParentheses2(string input)
+        {
+            int parentheses = 0;
+            foreach (char t in input)
+            {
+                if (t == '(')
+                {
+                    parentheses++;
+                }
+                else if (t == ')')
+                {
+                    parentheses--;
+
+                    if (parentheses < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parentheses == 0;
+        }
+    }
+}
